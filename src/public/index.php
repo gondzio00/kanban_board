6cbfaf2febcd2df79d84168d99691811b7e6e1b9
gondzio00 <gondzio00@gmail.com>
2020-03-05 23:49:08 +0100

    a

--- src/public/index.php
+++ src/public/index.php
@@ -11,9 +11,9 @@ $repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
 $authentication = new \KanbanBoard\Login();
 $token = $authentication->login();
 $github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
-$board = new \KanbanBoard\Application($github, $repositories, array('waiting-for-feedback'));
-$data = $board->board();
-$m = new Mustache_Engine(array(
-	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
-));
-echo $m->render('index', array('milestones' => $data));
+//$board = new \KanbanBoard\Application($github, $repositories, array('waiting-for-feedback'));
+//$data = $board->board();
+//$m = new Mustache_Engine(array(
+//	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
+//));
+//echo $m->render('index', array('milestones' => $data));
